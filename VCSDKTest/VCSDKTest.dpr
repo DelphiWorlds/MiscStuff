18f777b65a7b80eadf2500a3855399565d7d6c28
DelphiWorlds <dave@davenottage.com>
2018-07-25 10:28:29 +0930

    Fixed missing export

--- VCSDKTest/VCSDKTest.dpr
+++ VCSDKTest/VCSDKTest.dpr
@@ -10,19 +10,60 @@ library VCSDKTest;
   with your DLL. To avoid using BORLNDMM.DLL, pass string information
   using PChar or ShortString parameters. }
 
-uses
-  System.SysUtils,
-  System.Classes,
-  Winapi.Windows;
-
 {$R *.res}
 
 {$LINK 'vdapi.obj'}
+
+uses
+  System.SysUtils, System.IOUtils, System.Classes,
+  Winapi.Windows;
+
+type
+  TFileWriter = class(TStreamWriter)
+  private
+    FStream: TStream;
+  public
+    constructor Create(const Filename: string; Append: Boolean = False); overload; virtual;
+    destructor Destroy; override;
+  end;
+
+{ TFileWriter }
+
+constructor TFileWriter.Create(const Filename: string; Append: Boolean);
+var
+  LShareMode: Word;
+begin
+  if TOSVersion.Platform <> TOSVersion.TPlatform.pfiOS then
+    LShareMode := fmShareDenyWrite
+  else
+    LShareMode := 0;
+  if (not TFile.Exists(Filename)) or (not Append) then
+    FStream := TFileStream.Create(Filename, fmCreate or LShareMode)
+  else
+  begin
+    FStream := TFileStream.Create(Filename, fmOpenWrite or LShareMode);
+    FStream.Seek(0, soEnd);
+  end;
+  inherited Create(FStream);
+end;
+
+destructor TFileWriter.Destroy;
+begin
+  FStream.Free;
+  inherited;
+end;
+
+var
+  FLog: TFileWriter;
 
 procedure _Load; stdcall; external name '_Load';
 
 procedure Log(const AMsg: string);
 begin
+  if FLog = nil then
+    FLog := TFileWriter.Create('C:\Temp\WSTest.log', True);
+  FLog.AutoFlush := True;
+  FLog.WriteLine(FormatDateTime('yyyy/mm/dd hh:nn:ss.zzz', Now) + ' TEST Citrix VC SDK: ' + AMsg);
   OutputDebugString(PChar('TEST Citrix VC SDK: ' + AMsg));
 end;
 
@@ -71,6 +112,9 @@ procedure _DriverGetLastError; stdcall;
 begin
 
 end;
+
+exports
+  Load index 1;
 
 begin
 end.
